dd7e3a68839a57d0e24618e1b009cd6f87ab6b5d
Desc <41481611+Descended@users.noreply.github.com>
2025-02-07 11:06:41 +0100

    Initial Commit

---
+++ src/main/java/io/github/akshayjankie/PointUtil.java
@@ -0,0 +1,34 @@
+package io.github.akshayjankie;
+
+
+/**
+ * @author Akshay Jankie
+ */
+
+public final class PointUtil {
+
+    private PointUtil() {
+    }
+
+
+    /**
+     * Multiply two integers together
+     * @param a the first number
+     * @param b the second number
+     * @return the result
+     */
+    public static final int multiply(int a, int b) {
+        return a * b;
+    }
+
+
+    /**
+     * Divide two integers
+     * @param a the first number
+     * @param b the second number
+     * @return the result
+     */
+    public static final int divide(int a, int b) {
+        return a / b;
+    }
+}
---
+++ src/main/java/io/github/akshayjankie/Util.java
@@ -0,0 +1,33 @@
+package io.github.akshayjankie;
+
+
+/**
+ *
+ * @author Akshay Jankie
+ */
+
+public final class Util {
+
+    private Util() {
+    }
+
+    /**
+     * Add two integers together
+     * @param a first number
+     * @param b second number
+     * @return the result
+     */
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * Subtract two integers
+     * @param a first number
+     * @param b second number
+     * @return the result
+     */
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+}
